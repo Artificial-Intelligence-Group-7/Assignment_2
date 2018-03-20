f79cd21518721b2c43ac2e5588b925e53e456776
Rishi Khajuriwala <rdkhajuriwala@wpi.edu>
2018-03-20 00:59:51 -0400

    Add files via upload

--- EM.py
+++ EM.py
@@ -1,10 +1,10 @@
 import csv
-
+import numpy as np
 
 readings_x=[]
 readings_y=[]
 
-with open('data.csv', 'r') as g:
+with open('sample EM data (1).csv', 'r') as g:
 	reader = csv.reader(g)
 	my_list = list(reader)
 # print (my_list)
@@ -14,3 +14,32 @@ for column in my_list:
 
 # print (readings_x)
 # print (readings_y)
+
+
+def step_1(readings):
+	cluster_1_ =np.mean(readings)
+	cluster_2 = (cluster_1/2)
+	cluster_3 = (2*cluster_1)
+	a = []
+	b= []
+	c =[]
+	for x in readings:
+		s = x -cluster_1
+		a.append(s)
+	# print (a)
+	a_length = len(a)
+	# print (a_length)
+	for x in readings:
+		s = x -cluster_2
+		b.append(s)
+	# print (a)
+	b_length = len(b)
+	# print (a_length)
+	for x in readings:
+		s = x -cluster_3
+		c.append(s)
+	# print (a)
+	c_length = len(c)
+	return a
+	return b
+	return c
